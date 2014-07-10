b04c3ebe48bced4c980d0c45670c5f9fb5efb61f
Dung Mai <dungmai145@gmail.com>
2014-07-10 16:34:31 +0700

    first commit

---
+++ content/team-match-detail.php
@@ -0,0 +1,150 @@
+<script type="text/javascript" src="http://gettopup.com/releases/latest/top_up-min.js"></script>
+
+<div id="top_center">
+<div id="title">
+    <h2>Tournament</h2>
+</div>
+<div id="time">
+    <span>The Internationnal 2014</span><br>
+    <span style="font-size: 22px;line-height: 19px;">20 </span><span> Jun</span><br>
+    <span style="font-size: 16px;">31 minnutes from now</span>
+</div>
+</div>
+<div id="img_border">
+<img src="images/thanh-ngang.png">
+</div>
+<div id="top_center">
+<div id="title">
+    <h2>Team</h2>
+</div>
+<div id="time_2">
+    <div id="box_center">
+        <div class="title_team">
+            <div class="title_team_1">
+                 <span class="fix_sp">EG</span><br>
+                 <span class="fix_sp"><i>46%</i></span>
+            </div>                                   
+        </div>
+        <div class="title_team">
+            <a href="#"><img src="images/team1.png" style="width: 81px; height: 65px;margin-top: 6px;
+"></a>
+        </div>
+        <div class="title_team">
+            <div class="title_team_2">
+                <span style="font-size: 13px">vs</span>
+            </div>
+
+
+        </div>
+        <div class="title_team" style="margin-left: 5px;">
+            <a href="#"><img src="images/team11.png" style="width: 81px; height:65px;margin-top: 6px;
+"></a>
+        </div>
+        <div class="title_team">
+            <div class="title_team_1">
+                 <span class="fix_sp">Na'Vi</span><br>
+                 <span class="fix_sp"><i>54%</i></span>
+            </div>                                   
+        </div>
+    </div>
+    <div id="box_list">
+        <div class="menu_center">
+            <a href="#">GAME 1</a>
+        </div>
+        <div class="menu_center">
+            <a href="#">GAME 2</a>
+        </div>
+        <div class="menu_center">
+            <a href="#">GAME 3</a>
+        </div>
+    </div>
+</div>
+</div>
+<div id="img_border">
+<img src="images/thanh-ngang.png">
+</div>
+<div id="top_center">
+<div id="title">
+    <h2>Line-up</h2>
+</div>
+<div id="time" style="float: left">
+    <div id="icon_line">
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Bane_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Alchemist_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Brewmaster_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Chaos_Knight_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Axe_icon.png" class="img_icon"></a>
+        </div>
+    </div>
+     <div id="icon_line_1">
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Bane_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Chaos_Knight_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Centaur_Warrunner_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Alchemist_icon.png" class="img_icon"></a>
+        </div>
+        <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Gyrocopter_icon.png" class="img_icon"></a>
+        </div>
+    </div>
+</div>
+</div>
+<div id="top_center_1">
+<div id="title">
+    <h2 style="color: black">16'</h2>
+</div>
+ <div id="time" style="float: left;margin-left: 109px;margin-top: 8px;margin-right: 20px;">
+     <div class="icon_detail">
+            <a href="#"><img src="hero_icon/Alchemist_icon.png" class="img_icon"></a>
+    </div>
+     <div class="icon_detail" style="padding: 5px;">
+          <a href="#"><img src="images/icon_arow_right.png" ></a>
+    </div>
+    <div class="icon_detail">
+        <a href="#"><img src="hero_icon/Gyrocopter_icon.png" class="img_icon"></a>
+    </div>
+ </div>
+ <div id="num_ratio">
+     <div class="icon_detail">
+         <span>0</span>
+    </div>
+     <div class="icon_detail" >
+          <span>-</span>
+    </div>
+    <div class="icon_detail">
+        <span>1</span>
+    </div>
+ </div>
+ <div id="icon-img-popup">
+     <a href="images/img_match/match_1.jpg"><img src="images/icon_arow.png" class="img_icon" /></a> 
+     <a href="images/img_match/match_2.jpg"></a>
+      <a href="images/img_match/match_3.jpg"></a>
+ </div>
+ <!-- Popup Slide -->
+  <script type="text/javascript">
+        TopUp.addPresets({
+          "#icon-img-popup a": {
+            title: "Gallery {alt} ({current} of {total})",
+            group: "icon-img-popup",
+            readAltText: 1,
+            shaded: 1
+          }
+        });
+      </script>
+ <!-- End Popup Slide -->
+</div>
